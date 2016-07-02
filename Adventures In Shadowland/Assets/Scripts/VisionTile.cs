608b9c80ce3b68b2c0491349c9c49e61d7a4813a
joapet99 <joakimpokemeg@hotmail.no>
2016-07-02 11:30:01 +0200

    Saved :D

---
+++ Adventures In Shadowland/Assets/Scripts/VisionTile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionTile : MonoBehaviour{
+
+	public GameObject visionTile;
+	public bool visable;
+
+	public VisionTile(GameObject _visionTile, bool _visable)
+	{
+		this.visionTile = _visionTile;
+		this.visable = _visable;
+	}
+
+}
