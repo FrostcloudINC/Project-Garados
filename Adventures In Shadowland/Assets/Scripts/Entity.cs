dbc5fca3a0f74a837a52058c5e4cbd86bbdc23d9
joapet99 <joakimpokemeg@hotmail.no>
2016-07-04 14:56:04 +0200

    Added init function and used that function as the only action in the Start function.
    I did this so you could override and refer to init instead of needing to write that one sentence in every Start function of the future subclasses

--- Adventures In Shadowland/Assets/Scripts/Entity.cs
+++ Adventures In Shadowland/Assets/Scripts/Entity.cs
@@ -1,33 +1,70 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Entity : MonoBehaviour {
     List<EntityComponent> components;
 
-    public Entity() {
-        this.components = new List<EntityComponent>();
-    }
-
 	// Use this for initialization
 	void Start () {
+        init();
+    }
 
-	}
+    public void init() {
+        this.components = new List<EntityComponent>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        foreach(EntityComponent comp in this.components) {
+            comp.update();
+        }
 	}
 
-    void addComponent(EntityComponent comp) {
+    public void addComponent(EntityComponent comp) {
+        if(comp == null) {
+            Debug.LogError("The component that was gonna be added was equal to null");
+            return;
+        }
         this.components.Add(comp);
     }
 
-    void removeComponent(EntityComponent comp) {
+    public void removeComponent(EntityComponent comp) {
         this.components.Remove(comp);
     }
 
-    List<EntityComponent> getComponents() {
+    public List<EntityComponent> getComponents() {
         return components;
+    }
+
+    public bool hasComponentOfType(string type) {
+        Type t = Type.GetType(type);
+        if(t == null) {
+            Debug.LogWarning("No such type:" + type);
+            return false;
+        }
+        foreach(EntityComponent comp in components) {
+            if (comp.GetType().Equals(t)) {
+                return true;
+            }
+        }
+        Debug.LogWarning("Did NOT find component of type:" + t.Name + " | in gameobject:" + this.gameObject);
+        return false;
+    }
+
+    public EntityComponent getComponentOfType(string type) {
+        Type t = Type.GetType(type);
+        if (t == null) {
+            Debug.LogWarning("No such type:" + type);
+            return null;
+        }
+        foreach (EntityComponent comp in components) {
+            if (comp.GetType().Equals(t)) {
+                return comp;
+            }
+        }
+        Debug.LogWarning("Did NOT find component of type:" + t.Name + " | in gameobject:" + this.gameObject);
+        return null;
     }
 }
