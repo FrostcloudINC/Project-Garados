a20dd14fdeb3a67776fd88044a9fab94dbe3bb4e
joapet99 <joakimpokemeg@hotmail.no>
2016-07-04 13:05:31 +0200

    Added Entity superclass fixed #2

---
+++ Adventures In Shadowland/Assets/Scripts/Entity.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Entity : MonoBehaviour {
+    List<EntityComponent> components;
+
+    public Entity() {
+        this.components = new List<EntityComponent>();
+    }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void addComponent(EntityComponent comp) {
+        this.components.Add(comp);
+    }
+
+    void removeComponent(EntityComponent comp) {
+        this.components.Remove(comp);
+    }
+
+    List<EntityComponent> getComponents() {
+        return components;
+    }
+}
