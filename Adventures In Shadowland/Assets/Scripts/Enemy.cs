5dd47014d39994acb42f0d3872e65fb0ad8dcabe
joapet99 <joakimpokemeg@hotmail.no>
2016-07-04 15:00:44 +0200

    fixed #3
    Created Player and Enemy class as subclasses of Entity and tested these classes on TestLogicPlayer and TestLogicEnemy.
    When you start the demo the TestLogicEnemy will "attack" the TestLogicPlayer until the TestLogicPlayer is "dead".
    You can follow the logging when you start the game to see this.

---
+++ Adventures In Shadowland/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : Entity{
+    Health health;
+
+    void Start () {
+        init();
+    }
+
+    public new void init() {
+        base.init();
+        this.addComponent(health = new Health(3));
+        health.initHealthBar(this.gameObject);
+    }
+
+    void Update () {
+        attack(GameObject.Find("TestLogicPlayer").GetComponent<Entity>());
+    }
+
+    //TODO This method should be deleted when starting to make attack logic. This should be connected to it's own component
+    //This is just to test the Health system
+    void attack(Entity ent) {
+        if (ent.hasComponentOfType("Health")){
+            Health targetHealth = (Health)ent.getComponentOfType("Health");
+            if(targetHealth.getHealth() > 0) {
+                targetHealth.receiveDamage(1);
+            }
+        }
+    }
+}
---
+++ Adventures In Shadowland/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity{
+    Health health;
+
+    void Start() {
+        init();
+    }
+
+    public new void init() {
+        base.init();
+        this.addComponent(health = new Health(3));
+        health.initHealthBar(this.gameObject);
+    }
+}
