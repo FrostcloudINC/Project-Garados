970ed8a156577342d4c9e6ab3afd79275fb75db5
joapet99 <joakimpokemeg@hotmail.no>
2016-07-06 18:20:09 +0200

    Added Healthbar superclass

---
+++ Adventures In Shadowland/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Healthbar{
+    public Texture2D[] textures;
+
+    public abstract void init(Health h);
+    public abstract void update(Health h);
+}
