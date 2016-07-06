d42db725ed89056b07849d65e3acb47979171b5e
joapet99 <joakimpokemeg@hotmail.no>
2016-07-06 18:47:30 +0200

    Added Resources folder and created the subfolders Prefabs and Sprites. Moved all the sprites from previous Sprites folder to the new one. Also maded prefabs based on heart_full sprite and heart_half sprite

---
+++ Adventures In Shadowland/Assets/Scripts/PlayerHB.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class PlayerHB : HealthBar {
+    readonly int StartX = -50;
+    readonly int StartY = -20;
+    readonly int HeartWidth = 40;
+    readonly int HeartHeight = 40;
+    Canvas canvas;
+
+    public override void init(Health h) {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        int x = StartX;
+        int y = StartY;
+        for (int i = 0; i < h.getHealth(); i++) {
+            GameObject heart = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Heart_full"));
+            heart.transform.SetParent(GameObject.Find("Canvas").transform);
+            RectTransform transform = heart.GetComponent<RectTransform>();
+            transform.anchoredPosition3D = new Vector3(x, y);
+            transform.sizeDelta = new Vector2(HeartWidth, HeartHeight);
+            transform.localScale = new Vector3(1, 1, 1);
+            x -= HeartWidth;
+        }
+    }
+
+    public override void update(Health h) {
+        //TODO Update healthbar
+    }
+}
